8ae6037e38f15855501b5daa5d3c772024e6e422
tuf46437 <46876887+tuf46437@users.noreply.github.com>
2019-01-28 00:03:04 -0500

    Add files via upload

---
+++ SuccessfulSignUps.java
@@ -0,0 +1,27 @@
+package temple.edu;
+
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Button;
+public class SuccessfulSignUps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_successful_sign_ups);
+        Intent intent = getIntent();
+        String extra = intent.getExtras().getString("epuzzle");
+
+        String message =
+                        "Welcome, " +  extra + ", to the Sign Up Form App";
+        TextView tb = (TextView) findViewById(R.id.Message);
+        tb.setText(message);
+
+    }
+}
